954eaf521d3719e6ab4478e950981b49f1bf3485
Cristhian Orosco <u201921031@upc.edu.pe>
2023-06-17 18:36:32 -0500

    feat(comment):add packages and class

---
+++ lib/views/Administrator/ListComments.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ListComments extends StatefulWidget {
+  const ListComments({super.key});
+
+  @override
+  State<ListComments> createState() => _ListCommentsState();
+}
+
+class _ListCommentsState extends State<ListComments> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/views/Client/AddCommentView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddCommentView extends StatefulWidget {
+  const AddCommentView({super.key});
+
+  @override
+  State<AddCommentView> createState() => _AddCommentViewState();
+}
+
+class _AddCommentViewState extends State<AddCommentView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
